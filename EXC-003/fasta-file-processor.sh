4f02cbc17f2d0c6274f641d12767c3e4a550216f
anastasiaplaettner <anastasiaplaettner@gmail.com>
2025-02-10 13:29:07 +0100

    final version

--- EXC-003/fasta-file-processor.sh
+++ EXC-003/fasta-file-processor.sh
@@ -1,12 +1,14 @@
 FASTA_FILE=$1
 
+
 num_sequences=$(grep -c ">" "$1")
 seq_length=$(grep -v ">" "$1" | paste -sd "" - | awk '{print length}')
 total_length=$(echo "$seq_length" | awk '{sum+=$1} END {print sum}')
 longest_seq=$(awk '!/>/ {if (length > max) max = length} END {print max}' "$1")
-shortest_seq=$(awk '!/>/ {if (length < min) min = length} END {print min}' "$1")
+shortest_seq=$(awk '!/>/ {if (min == 0 || length < min) min = length} END {print min}' "$1")
 avg_length=$(awk "BEGIN {print $total_length/$num_sequences}")
-gc_percent=$(grep -v ">" "$1" | grep -o "[GCgc]" | wc -l)
+gc_content=$(grep -v ">" "$1" | grep -o "[GCgc]" | wc -l)
+gc_percent=$(awk "BEGIN {print ($gc_content/$total_length)*100}")
 
 
  
